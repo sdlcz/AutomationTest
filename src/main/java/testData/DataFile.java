974a2781ff9b6a62ef0683d214eac3343e553d8b
sdlcz <132066587+sdlcz@users.noreply.github.com>
2023-06-28 14:30:07 +0100

    Update Data

--- src/main/java/testData/DataFile.java
+++ src/main/java/testData/DataFile.java
@@ -6,6 +6,7 @@ public class DataFile {
 	public static String homePageURL ="https://www.prospects.ac.uk/";
 	public static String registrationPageURL ="https://www.prospects.ac.uk/register";
 	public static String gettingAJobPageURL = "https://www.prospects.ac.uk/careers-advice/getting-a-job";
+	public static String upcomingWebinarsPageURL = "https://www.gradcracker.com/career-centre/webinars/upcoming";
 	
 	//Registration Page
 	public static String userEmail = "joe.blogs@contoso.com";
@@ -13,5 +14,7 @@ public class DataFile {
 	public static String userLastName = "Blogs";
 	public static String registrationPageHeading = "Registration: Step 1 of 2";
 	public static String chooseResponse = "Yes";
+	
+	
 
 }
