4b5158fa1aeb9559d8a2848f6582f6b4033ca9fc
sdlcz <132066587+sdlcz@users.noreply.github.com>
2023-06-28 14:33:51 +0100

    Add Test case 3

---
+++ src/main/java/testSuite/testCase3.java
@@ -0,0 +1,43 @@
+package testSuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import driverUtilities.DriverUtilities;
+import junit.framework.Assert;
+import pageObjectModels.Navigation;
+import testData.DataFile;
+
+public class testCase3 {
+
+	WebDriver driver;
+	
+@Before
+public void preConditions() {
+	DriverUtilities myDriverUtilities = new DriverUtilities();
+	driver = myDriverUtilities.getDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.gradcracker.com/");
+	
+}
+
+@Test
+public void testInterviewTips() {
+	Actions hoverCareers = new Actions(driver);
+	hoverCareers.moveToElement(Navigation.careersCentre(driver)).perform();
+	Navigation.upcomingWebinars(driver).isDisplayed();
+	
+	Navigation.upcomingWebinars(driver).click();
+	Assert.assertEquals(DataFile.upcomingWebinarsPageURL, driver.getCurrentUrl());
+	
+}
+
+@After
+public void teardown() {
+	//driver.quit
+}
+
+}
