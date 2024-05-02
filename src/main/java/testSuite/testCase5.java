414b661857ec8de4d7c70c16f8871657eadc0a06
sdlcz <stantonsdlc@gmail.com>
2024-05-02 11:53:53 +0100

    testCase5 junit

--- src/main/java/testSuite/testCase5.java
+++ src/main/java/testSuite/testCase5.java
@@ -1,18 +1,25 @@
 package testSuite;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class testCase5 {
 	WebDriver driver;
 	
+	@Before
 	public void preConditions() {	
 		
 	}
 	
+	@Test
 	public void testLoginPage() {
 		
 	}
 	
+	@After
 	public void tearDown() {
 		
 	}
