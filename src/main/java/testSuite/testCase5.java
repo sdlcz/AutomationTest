8ef1fe1688e15900f02e27b6022e8eee5f9346a2
sdlcz <132066587+sdlcz@users.noreply.github.com>
2024-05-24 17:01:50 +0100

    Include positive test

--- src/main/java/testSuite/testCase5.java
+++ src/main/java/testSuite/testCase5.java
@@ -1,19 +1,50 @@
 package testSuite;
 
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import driverUtilities.DriverUtilities;
+import pageObjectModels.Navigation;
+import pageObjectModels.LoginPage;
+import testData.DataFile;
+import usefullMethods.ScreenShots;
 
 public class testCase5 {
 	WebDriver driver;
 	
+	@Before
 	public void preConditions() {	
+		DriverUtilities myDriverUtilities = new DriverUtilities();
+		driver = myDriverUtilities.getDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
 		
 	}
 	
+	@Test
 	public void testLoginPage() {
+		Assert.assertEquals("https://www.saucedemo.com/", driver.getCurrentUrl());
 		
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+		Assert.assertEquals("standard_user", driver.findElement(By.xpath("//*[@id=\"user-name\"]")).getAttribute("value"));
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		Assert.assertEquals("secret_sauce",driver.findElement(By.xpath("//*[@id=\"password\"]")).getAttribute("value"));
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		
+		Assert.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
 	}
 	
+	@After
 	public void tearDown() {
+		driver.quit();
 		
 	}
 	
