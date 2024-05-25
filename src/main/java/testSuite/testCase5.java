c7dac5cedd2a35bf9aef9543e2721957c69860fe
sdlcz <132066587+sdlcz@users.noreply.github.com>
2024-05-25 12:59:56 +0100

    Negative test

--- src/main/java/testSuite/testCase5.java
+++ src/main/java/testSuite/testCase5.java
@@ -32,7 +32,7 @@ public class testCase5 {
 	@Test
 	public void testLoginPage() {
 		Assert.assertEquals("https://www.saucedemo.com/", driver.getCurrentUrl());
-		
+		//Positive test
 		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
 		Assert.assertEquals("standard_user", driver.findElement(By.xpath("//*[@id=\"user-name\"]")).getAttribute("value"));
 		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
@@ -40,6 +40,13 @@ public class testCase5 {
 		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 		
 		Assert.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+		driver.navigate().back();
+		
+		//Negative test
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("test_user");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).isDisplayed());
 	}
 	
 	@After
